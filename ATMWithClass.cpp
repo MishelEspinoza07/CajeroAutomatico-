123eda24a4dfde5b1b4c9a145e1d58fae5ff8ed2
tarnishedAlexander <alex4750384@gmail.com>
2024-02-08 04:22:10 +0000

    Update the code with class and whitout class

--- ATMWithClass.cpp
+++ ATMWithClass.cpp
@@ -4,11 +4,17 @@
 #include <cstdlib>
 using namespace std;
 
-int Verifypassword() {
-    int password;
-    int correctPasword = 1234;
-    int fails = 3;
+class Account{
+    public:
+    int correctPassword;
+    int fails;
+    Account(int correctPass, int failsCount){
+        correctPassword = correctPass;
+        fails = failsCount;
+    }
 
+    int Verifypassword() {
+    int password;
     cout<<"Ingrese la contraseña: \n";
     while (fails) {
         if (!(cin >> password)) { 
@@ -16,7 +22,7 @@ int Verifypassword() {
             cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
             cout << "Por favor, ingrese solo números.\n";
         }
-        else if (correctPasword == password) {
+        else if (correctPassword == password) {
             return 1;
         } else {
             fails--;
@@ -25,9 +31,11 @@ int Verifypassword() {
     }
     return 0;
 }
+};
 
-
-int addMoney(){
+class ATM{
+public:
+    int addMoney(){
     int mount;
     cout << "Monto a depositar: \n";
     while (!(cin >> mount)) {
@@ -46,19 +54,28 @@ void balance(int money){
 int withdraw(int money){
     int mount;
     cout<< "Cuanto desea retirar:\n";
-    cin >> mount;
+    while (!(cin >> mount)) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Por favor, ingrese solo números.\n";
+        cout << "Monto a retirar: \n";
+    }
     if(money>=mount){
         return mount;
     }
     cout<<"Su retiro excede el saldo que tiene actualmente\n";
     return 0;
 }
+};
 
 int main(){
     int options;
     int money = 0;
     bool end = 1;
-    if(Verifypassword()){
+    Account account = Account(1234,3);
+    ATM atm = ATM();
+
+    if(account.Verifypassword()){
         do {
             cout << "----------------------------------------------------------------\n";
             cout << "Elige una de las siguientes opciones opciones:\n";
@@ -71,15 +88,15 @@ int main(){
         switch (options)
         {
         case 1:
-            money += addMoney();
-            balance(money);
+            money += atm.addMoney();
+            atm.balance(money);
             break;
         case 2:
-            money -= withdraw(money);
-            balance(money);
+            money -= atm.withdraw(money);
+            atm.balance(money);
             break;
         case 3:
-            balance(money);
+            atm.balance(money);
             break;
         case 4:
             end = 0;
@@ -90,7 +107,7 @@ int main(){
         }
     }while (end);
     }else{
-        cout<<"Se acabo el numero de intentos\n";
+        cout<<"no puede ser\n";
     }
     
 
